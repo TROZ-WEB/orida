bd47b0a718377dded41de2b32550096b7f6bad3f
Agathe Guillemot <50427309+agguillemot@users.noreply.github.com>
2022-05-16 17:46:35 +0200

    feat: add number of responses to poll results (#109)

--- frontend/src/components/PollResponse/index.tsx
+++ frontend/src/components/PollResponse/index.tsx
@@ -4,12 +4,14 @@ import useThunkDispatch from '@hooks/useThunkDispatch';
 import { Poll } from '@services/polls';
 import { getResult } from '@store/polls/actions';
 import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import PollResultsRow from './PollResponseRow';
 
 const classes = {
     card: 'bg-white rounded-lg p-5 pb-3 h-full flex flex-col justify-between',
     question: 'text-lg text-primary-dark leading-6 mb-4 block min-h-[48px]',
+    responses: 'text-sm text-neutral-400 mt-2',
 };
 
 interface PollResultsProps {
@@ -17,6 +19,7 @@ interface PollResultsProps {
 }
 
 const PollResults = ({ poll }: PollResultsProps) => {
+    const { t } = useTranslation();
     const dispatch = useThunkDispatch();
     const data = useSelector((state) =>
         state.polls.results.find((result) => result.pollId === poll.id)
@@ -41,6 +44,9 @@ const PollResults = ({ poll }: PollResultsProps) => {
                             />
                         ))}
                     </div>
+                    <span className={classes.responses}>
+                        {data.responses.length} {t('poll_results_respondants')}
+                    </span>
                 </>
             ) : (
                 <Loader />
